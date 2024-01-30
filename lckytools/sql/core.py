48a4ac97c2efba3ad6713f23931067d2ff72f3fc
LCKYN <T.Pawarit@lckyn.com>
2024-01-30 12:14:26 +0700

    Update create_table_sql function to include 'ignore_if_exist' parameter and increment version to 0.0.4

--- lckytools/sql/core.py
+++ lckytools/sql/core.py
@@ -8,21 +8,22 @@ TYPE_MAPPING = {
 }
 
 
-def create_table_sql(table_name, schema_input):
+def create_table_sql(table_name, schema_input, ignore_if_exist=False):
     if isinstance(schema_input, list):
         columns = [
             f"{col_name} {TYPE_MAPPING.get(data_type, data_type)}"
             for col_name, data_type in schema_input
         ]
-
     elif isinstance(schema_input, pd.DataFrame):
         columns = [
             f"{col_name} {TYPE_MAPPING.get(str(dtype), str(dtype))}"
             for col_name, dtype in zip(schema_input.columns, schema_input.dtypes)
         ]
-
     else:
         raise ValueError("Invalid schema_input format")
 
     columns_str = ", ".join(columns)
-    return f"CREATE TABLE {table_name} ({columns_str});"
+    if ignore_if_exist:
+        return f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_str});"
+    else:
+        return f"CREATE TABLE {table_name} ({columns_str});"
