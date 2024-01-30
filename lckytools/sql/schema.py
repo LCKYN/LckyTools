434712616b4ff6d304472d7c8423adf12571b49b
LCKYN <T.Pawarit@lckyn.com>
2024-01-30 13:25:26 +0700

    Update schema.py and pyproject.toml versions

--- lckytools/sql/schema.py
+++ lckytools/sql/schema.py
@@ -5,16 +5,11 @@ def create_schema(catalog_name, schema_name, ignore_if_exist=False):
         return f"CREATE SCHEMA {catalog_name}.{schema_name};"
 
 
-def drop_schema(catalog_name, schema_name, ignore_if_not_exist=False, cascade=False):
+def drop_schema(catalog_name, schema_name, ignore_if_not_exist=False):
     if ignore_if_not_exist:
         sql = f"DROP SCHEMA IF EXISTS {catalog_name}.{schema_name}"
     else:
         sql = f"DROP SCHEMA {catalog_name}.{schema_name}"
-
-    if cascade:
-        sql += " CASCADE"
-    else:
-        sql += " RESTRICT"
 
     return f"{sql};"
 
