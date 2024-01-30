3b67fe900e462e0902cd5eb930cbd5a7806a32a5
LCKYN <T.Pawarit@lckyn.com>
2024-01-30 12:48:18 +0700

    Update schema creation to include catalog name and bump version

--- lckytools/sql/schema.py
+++ lckytools/sql/schema.py
@@ -1,8 +1,8 @@
-def create_schema(schema_name, ignore_if_exist=False):
+def create_schema(catalog_name, schema_name, ignore_if_exist=False):
     if ignore_if_exist:
-        return f"CREATE SCHEMA IF NOT EXISTS {schema_name};"
+        return f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.{schema_name};"
     else:
-        return f"CREATE SCHEMA {schema_name};"
+        return f"CREATE SCHEMA {catalog_name}.{schema_name};"
 
 
 def drop_schema(schema_name, ignore_if_not_exist=False, cascade=False):
